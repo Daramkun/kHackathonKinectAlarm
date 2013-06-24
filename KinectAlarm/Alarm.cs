b82bb3d89216caa3d8ec83efc228dd3cbeb2abbc
Jong Guk Choi <jjj1211@hanmir.com>
2013-06-25 02:07:40 +0900

    asfd
    asfd

---
+++ KinectAlarm/Alarm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectAlarm
+{
+    public class Alarm
+    {
+        public enum DateType { WeekDay = 0, Weekend = 1, DayByDay = 2 }
+        DateType dateType;
+        TimeSpan timeForAlarm;
+        String alarmMemo;
+
+        public Alarm(String inputDateType, int h, int m, String memo)
+        {
+            if (inputDateType == "WeekDay")
+                dateType = DateType.WeekDay;
+            else if(inputDateType = "
+
+            timeForAlarm = new TimeSpan(h, m, 0);
+            alarmMemo = memo;
+        }
+    }
+}
---
+++ KinectAlarm/AlarmCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace KinectAlarm
+{
+    public static class AlarmCollection
+    {
+        static List<Alarm> alarmList = new List<Alarm>();
+
+        public static void addAlarm(Alarm inputAlarm)
+        {
+            alarmList.Add(inputAlarm);
+        }
+
+        public static void removeAlarm(int index)
+        {
+            alarmList.RemoveAt(index);
+        }
+
+        public static async void loadData()
+        {
+            alarmList.Clear();
+            try
+            {
+                StorageFile storageFile = await StorageFile.GetFileFromPathAsync("alarmList.dat");
+                using (IRandomAccessStream raStream = await storageFile.OpenAsync(FileAccessMode.Read))
+                {
+                    DataReader reader = new DataReader(raStream);
+                    int dataLength = reader.ReadInt32();
+                    for (int i = 0; i < dataLength; i++)
+                    {
+                        Alarm alarm = new Alarm(reader.ReadString(255), reader.ReadInt32(), reader.ReadInt32(), reader.ReadString(255));\
+                        alarmList.Add(alarm);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
