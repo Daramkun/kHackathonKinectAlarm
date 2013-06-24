99637f013d4da98a278e3efaaba79abe94d1d12b
Jaeyeon Jin <daramkun@daram.pe.kr>
2013-06-24 20:53:13 +0900

    Worked Kinect test page

---
+++ KinectAlarm/Common/MultiplyConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace KinectAlarm.Common
+{
+	public class MultiplyConverter : IValueConverter
+	{
+		double AsDouble ( object value )
+		{
+			var valueText = value as string;
+			if ( valueText != null )
+				return double.Parse ( valueText );
+			else
+				return ( double ) value;
+		}
+
+		public object Convert ( object value, Type targetType, object parameter, string language )
+		{
+			return AsDouble ( value ) * AsDouble ( parameter );
+		}
+
+		public object ConvertBack ( object value, Type targetType, object parameter, string language )
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
---
+++ KinectAlarm/KinectPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using KinectData;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+namespace KinectAlarm
+{
+	public sealed partial class KinectPage : Page
+	{
+		KinectProviderServiceClient client;
+
+		public KinectPage ()
+		{
+			this.InitializeComponent ();
+			client = new KinectProviderServiceClient ();
+			var timer = new DispatcherTimer ();
+			timer.Interval = TimeSpan.FromTicks ( 333333 );
+			timer.Tick += async ( object sender, object e ) =>
+			{
+				Kinect data = await client.GetDataAsync ( new Kinect () );
+				SetPosition ( boneHead, data.Skeleton [ ( int ) Kinect.JointType.Head ] );
+				SetPosition ( boneShoulderCenter, data.Skeleton [ ( int ) Kinect.JointType.ShoulderCenter ] );
+				SetPosition ( boneShoulderLeft, data.Skeleton [ ( int ) Kinect.JointType.ShoulderLeft ] );
+				SetPosition ( boneShoulderRight, data.Skeleton [ ( int ) Kinect.JointType.ShoulderRight ] );
+				SetPosition ( boneSpine, data.Skeleton [ ( int ) Kinect.JointType.Spine ] );
+				SetPosition ( boneHipCenter, data.Skeleton [ ( int ) Kinect.JointType.HipCenter ] );
+				SetPosition ( boneHipLeft, data.Skeleton [ ( int ) Kinect.JointType.HipLeft ] );
+				SetPosition ( boneHipRight, data.Skeleton [ ( int ) Kinect.JointType.HipRight ] );
+				SetPosition ( boneKneeLeft, data.Skeleton [ ( int ) Kinect.JointType.KneeLeft ] );
+				SetPosition ( boneKneeRight, data.Skeleton [ ( int ) Kinect.JointType.KneeRight ] );
+				SetPosition ( boneAnkleLeft, data.Skeleton [ ( int ) Kinect.JointType.AnkleLeft ] );
+				SetPosition ( boneAnkleRight, data.Skeleton [ ( int ) Kinect.JointType.AnkleRight ] );
+				SetPosition ( boneFootLeft, data.Skeleton [ ( int ) Kinect.JointType.FootLeft ] );
+				SetPosition ( boneFootRight, data.Skeleton [ ( int ) Kinect.JointType.FootRight ] );
+				SetPosition ( boneElbowLeft, data.Skeleton [ ( int ) Kinect.JointType.ElbowLeft ] );
+				SetPosition ( boneElbowRight, data.Skeleton [ ( int ) Kinect.JointType.ElbowRight ] );
+				SetPosition ( boneWristLeft, data.Skeleton [ ( int ) Kinect.JointType.WristLeft ] );
+				SetPosition ( boneWristRight, data.Skeleton [ ( int ) Kinect.JointType.WristRight ] );
+				SetPosition ( boneHandLeft, data.Skeleton [ ( int ) Kinect.JointType.HandLeft ] );
+				SetPosition ( boneHandRight, data.Skeleton [ ( int ) Kinect.JointType.HandRight ] );
+
+			};
+			timer.Start ();
+		}
+
+		private void SetPosition ( Ellipse eliipse, Kinect.Joint joint )
+		{
+			Canvas.SetLeft ( eliipse, joint.X * 200 );
+			Canvas.SetTop ( eliipse, -joint.Y * 200 );
+		}
+
+		protected override void OnNavigatedTo ( NavigationEventArgs e )
+		{
+
+		}
+	}
+}
